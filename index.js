f010e465f3a4b20c40bc4d5bc2d8916a7c83dd4a
victor-divo-m <victordivo3133@gmail.com>
2024-09-23 19:07:25 +0700

    change browser name based on env

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const Pepesan = require("pepesan");
 const router = require("./router");
-const { ALLOWED_NUMBERS } = process.env;
+const { ALLOWED_NUMBERS, BROWSER_NAME } = process.env;
 
 (async () => {
     const config = {
         allowedNumbers: ALLOWED_NUMBERS ? ALLOWED_NUMBERS.split(',') : null,
-        browserName: 'Dewakoding App'
+        browserName: BROWSER_NAME || 'BOT_WA'
     }
     const pepesan = Pepesan.init(router, config)
     await pepesan.connect()
